6440b2f6f37c99b988841d6f960d97f8f4d9e98e
Jeremy SCHOTTE <jschotte@e3r5p10.42.fr>
2018-03-15 15:35:03 +0100

    init project

---
+++ swingy/src/main/java/com/jschotte/swingy/swingy/App.java
@@ -0,0 +1,13 @@
+package com.jschotte.swingy.swingy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
